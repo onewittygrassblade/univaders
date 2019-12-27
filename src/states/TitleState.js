6613170f8e57fda5f5a3187657ee1157e7306e77
OneWittyGrassBlade <jillphys@hotmail.com>
2019-12-27 18:29:06 +0100

    Increase font size

--- src/states/TitleState.js
+++ src/states/TitleState.js
@@ -1,8 +1,8 @@
-import { Container, BitmapText } from '../const/aliases';
+import { BitmapText } from '../const/aliases';
 
 import State from './State';
 
-import { RENDERER_WIDTH, RENDERER_HEIGHT } from '../const/app';
+import { RENDERER_WIDTH } from '../const/app';
 
 export default class TitleState extends State {
   constructor(stateStack, context) {
@@ -12,16 +12,16 @@ export default class TitleState extends State {
   }
 
   createTitle() {
-    const title = new BitmapText('univaders', { font: '72px arcade-lowercase-white' });
+    const title = new BitmapText('univaders', { font: '180px arcade-lowercase-white' });
     title.x = RENDERER_WIDTH / 2 - title.width / 2;
-    title.y = 120;
+    title.y = 200;
     this.container.addChild(title);
   }
 
   createHint() {
-    const hint = new BitmapText('press space to start', { font: '48px arcade-lowercase-white' });
+    const hint = new BitmapText('press space to start', { font: '72px arcade-lowercase-white' });
     hint.x = RENDERER_WIDTH / 2 - hint.width / 2;
-    hint.y = 320;
+    hint.y = 400;
     this.container.addChild(hint);
   }
 
